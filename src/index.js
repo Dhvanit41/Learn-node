afd617d943d6ee30f91a166aa70e701aa137f0b4
Dhvanit popat <dhvanit.popat@byteprpophecy.com>
2020-10-25 19:34:36 +0530

    added cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+var cors = require('cors')
 
 require("./db/mongoose");
 const User = require("./models/User");
@@ -10,6 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 app.use(userRouter);
+app.use(cors());
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
